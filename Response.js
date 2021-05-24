325114b5cb1ecace81db5c160b17afb460866622
tronexis <tronexis>
2021-05-24 12:34:11 +0600

    add: response, edit: search

---
+++ Response.js
@@ -0,0 +1,471 @@
+export default {
+  kind: "customsearch#search",
+  url: {
+    type: "application/json",
+    template:
+      "https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json",
+  },
+  queries: {
+    request: [
+      {
+        title: "Google Custom Search - ok google",
+        totalResults: "583000000",
+        searchTerms: "ok google",
+        count: 10,
+        startIndex: 1,
+        inputEncoding: "utf8",
+        outputEncoding: "utf8",
+        safe: "off",
+        cx: "c3cd785e86be2a292",
+      },
+    ],
+    nextPage: [
+      {
+        title: "Google Custom Search - ok google",
+        totalResults: "583000000",
+        searchTerms: "ok google",
+        count: 10,
+        startIndex: 11,
+        inputEncoding: "utf8",
+        outputEncoding: "utf8",
+        safe: "off",
+        cx: "c3cd785e86be2a292",
+      },
+    ],
+  },
+  context: {
+    title: "Google",
+  },
+  searchInformation: {
+    searchTime: 0.921613,
+    formattedSearchTime: "0.92",
+    totalResults: "583000000",
+    formattedTotalResults: "583,000,000",
+  },
+  items: [
+    {
+      kind: "customsearch#result",
+      title: "Mayes County, Oklahoma – Data Centers – Google",
+      htmlTitle: "Mayes County, <b>Oklahoma</b> – Data Centers – <b>Google</b>",
+      link: "https://www.google.com/about/datacenters/locations/mayes-county/",
+      displayLink: "www.google.com",
+      snippet:
+        "Art meets tech. On the exterior wall of our Oklahoma data center, digital artist \nJenny Odell has created collages made up of imagery collected from Google \nMaps.",
+      htmlSnippet:
+        "Art meets tech. On the exterior wall of our <b>Oklahoma</b> data center, digital artist <br>\nJenny Odell has created collages made up of imagery collected from <b>Google</b> <br>\nMaps.",
+      cacheId: "seKk1SvLDT8J",
+      formattedUrl:
+        "https://www.google.com/about/datacenters/locations/mayes-county/",
+      htmlFormattedUrl:
+        "https://www.<b>google</b>.com/about/datacenters/locations/mayes-county/",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPz5n_t5q9SVtFmxYmAZr5I-CG-jQIH8xP6EE_cz50Lh2ReU_Lrn5hVq8H",
+            width: "295",
+            height: "171",
+          },
+        ],
+        metatags: [
+          {
+            "og:type": "website",
+            "og:site_name": "Google Data Centers",
+            viewport: "initial-scale=1, minimum-scale=1, width=device-width",
+            "og:title": "Mayes County, Oklahoma – Data Centers – Google",
+            "og:url":
+              "https://www.google.com/about/datacenters/locations/mayes-county/",
+            "og:description":
+              "Google is proud to call Oklahoma home to one of our data centers.",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://lh3.googleusercontent.com/cs89KtU-p6cyMTLZnZfjCDDv_KZWrlrQrmuhTuUzJg4tEYAPbzEq1zJK-V5z_d1Z_iTLxk5vS_tMNBA7LSATYs3aLImKSUwLhUWNNiI=w600-l100-sg-rj-c0xffffff",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Claremore, Claremore, OK - Google Maps",
+      htmlTitle: "Claremore, Claremore, <b>OK</b> - <b>Google</b> Maps",
+      link: "https://www.google.com/maps/search/?api=1&query=Claremore%2C%20Claremore%2C%20OK&query_place_id=ChIJy9VjooJQtocRUFUS_6MxGwc",
+      displayLink: "www.google.com",
+      snippet:
+        "Claremore, Claremore, OK. Connect to internet to see place info.",
+      htmlSnippet:
+        "Claremore, Claremore, <b>OK</b>. Connect to internet to see place info.",
+      cacheId: "NdkgJOtTKmgJ",
+      formattedUrl:
+        "https://www.google.com/maps/search/?api=1&query...query...id...",
+      htmlFormattedUrl:
+        "https://www.<b>google</b>.com/maps/search/?api=1&amp;query...query...id...",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQUDTMfgWSotdHvbOKW4AmY1steqsilnDJS4q33zGx-d0AXvvxPiFY9Mvs",
+            width: "204",
+            height: "247",
+          },
+        ],
+        metatags: [
+          {
+            referrer: "origin",
+            "twitter:card": "summary",
+            "theme-color": "#3367d6",
+            viewport: "initial-scale=1.0, user-scalable=no",
+            google: "notranslate",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://maps.gstatic.com/tactile/basepage/pegman_sherlock.png",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Google Maps",
+      htmlTitle: "<b>Google</b> Maps",
+      link: "https://www.google.com/maps/@35.2046352,-97.4469721,16z",
+      displayLink: "www.google.com",
+      snippet:
+        "Find local businesses, view maps and get driving directions in Google Maps.",
+      htmlSnippet:
+        "Find local businesses, view maps and get driving directions in <b>Google</b> Maps.",
+      cacheId: "omGyW8Z-a5IJ",
+      formattedUrl: "https://www.google.com/maps/@35.2046352,-97.4469721,16z",
+      htmlFormattedUrl:
+        "https://www.<b>google</b>.com/maps/@35.2046352,-97.4469721,16z",
+      pagemap: {
+        metatags: [
+          {
+            referrer: "origin",
+            "og:image":
+              "https://maps.google.com/maps/api/staticmap?center=35.2046352%2C-97.4469721&zoom=16&size=256x256&language=en&sensor=false&client=google-maps-frontend&signature=YdQC6gNoNf-yvw3CRx8tPLPAQ0s",
+            "og:image:width": "256",
+            "twitter:card": "summary",
+            "og:site_name": "Google Maps",
+            viewport:
+              "initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no",
+            "og:title": "Google Maps",
+            "og:image:height": "256",
+            google: "notranslate",
+            "og:description":
+              "Find local businesses, view maps and get driving directions in Google Maps.",
+          },
+        ],
+        place: [
+          {
+            image:
+              "https://maps.google.com/maps/api/staticmap?center=35.2046352%2C-97.4469721&zoom=16&size=256x256&language=en&sensor=false&client=google-maps-frontend&signature=YdQC6gNoNf-yvw3CRx8tPLPAQ0s",
+            name: "Google Maps",
+            description:
+              "Find local businesses, view maps and get driving directions in Google Maps.",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://maps.google.com/maps/api/staticmap?center=35.2046352%2C-97.4469721&zoom=16&size=256x256&language=en&sensor=false&client=google-maps-frontend&signature=YdQC6gNoNf-yvw3CRx8tPLPAQ0s",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Google Maps",
+      htmlTitle: "<b>Google</b> Maps",
+      link: "https://www.google.com/maps/contrib/104234102833100983403/reviews",
+      displayLink: "www.google.com",
+      snippet:
+        "You can contribute on Google Maps too! See my profile. Reviews Photos ... \nBrownie's Hamburger Stand. 2130 S Harvard Ave, Tulsa, OK 74114, United \nStates -.",
+      htmlSnippet:
+        "You can contribute on <b>Google</b> Maps too! See my profile. Reviews Photos ... <br>\nBrownie&#39;s Hamburger Stand. 2130 S Harvard Ave, Tulsa, <b>OK</b> 74114, United <br>\nStates -.",
+      cacheId: "4SH8Yiah0gIJ",
+      formattedUrl:
+        "https://www.google.com/maps/contrib/104234102833100983403/reviews",
+      htmlFormattedUrl:
+        "https://www.<b>google</b>.com/maps/contrib/104234102833100983403/reviews",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSjsuMXw41HvKt-V5343CUF8T9SctSxlub6gOjFnmpdNLm0us0e5clNujI",
+            width: "310",
+            height: "163",
+          },
+        ],
+        metatags: [
+          {
+            referrer: "origin",
+            "og:image":
+              "https://maps.gstatic.com/tactile/geoprofile/sharing/non_lg.png",
+            "og:image:width": "256",
+            "twitter:card": "summary",
+            "og:site_name": "Contributions by Grant Wilburn",
+            viewport:
+              "initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no",
+            "og:title": "Contributions by Grant Wilburn",
+            "og:image:height": "256",
+            google: "notranslate",
+            "og:description": "713 Contributions",
+          },
+        ],
+        place: [
+          {
+            image:
+              "https://maps.gstatic.com/tactile/geoprofile/sharing/non_lg.png",
+            name: "Contributions by Grant Wilburn",
+            description: "713 Contributions",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://maps.gstatic.com/tactile/geoprofile/sharing/non_lg.png",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Peyton's Place, 10298 OK-10, Tahlequah, OK - Google Maps",
+      htmlTitle:
+        "Peyton&#39;s Place, 10298 OK-10, Tahlequah, <b>OK</b> - <b>Google</b> Maps",
+      link: "https://www.google.com/maps/search/?api=1&query=Peyton%27s%20Place%2C%2010298%20OK-10%2C%20Tahlequah%2C%20OK&query_place_id=ChIJoYBuMAnlyYcRp8LJO2Hi8pE",
+      displayLink: "www.google.com",
+      snippet:
+        "Peyton's Place, 10298 OK-10, Tahlequah, OK. Your location. Trails. Dedicated \nlanes. Bicycle-friendly roads. Dirt/unpaved trails. Live traffic. Fast. Slow. 2000 km.",
+      htmlSnippet:
+        "Peyton&#39;s Place, 10298 <b>OK</b>-10, Tahlequah, <b>OK</b>. Your location. Trails. Dedicated <br>\nlanes. Bicycle-friendly roads. Dirt/unpaved trails. Live traffic. Fast. Slow. 2000 km.",
+      cacheId: "KbeQ6UebdGQJ",
+      formattedUrl:
+        "https://www.google.com/maps/search/?api=1&query...query...id...",
+      htmlFormattedUrl:
+        "https://www.<b>google</b>.com/maps/search/?api=1&amp;query...query...id...",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQUDTMfgWSotdHvbOKW4AmY1steqsilnDJS4q33zGx-d0AXvvxPiFY9Mvs",
+            width: "204",
+            height: "247",
+          },
+        ],
+        metatags: [
+          {
+            referrer: "origin",
+            "twitter:card": "summary",
+            "theme-color": "#3367d6",
+            viewport: "initial-scale=1.0, user-scalable=no",
+            google: "notranslate",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://maps.gstatic.com/tactile/basepage/pegman_sherlock.png",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Ruby Ranch Boutique, 316 W Main St, Tishomingo, OK - Google Maps",
+      htmlTitle:
+        "Ruby Ranch Boutique, 316 W Main St, Tishomingo, <b>OK</b> - <b>Google</b> Maps",
+      link: "https://www.google.com/maps/search/?api=1&query=Ruby%20Ranch%20Boutique%2C%20316%20W%20Main%20St%2C%20Tishomingo%2C%20OK&query_place_id=ChIJ5_oJYZq3TIYRGhSw1YZrerY",
+      displayLink: "www.google.com",
+      snippet:
+        "Ruby Ranch Boutique, 316 W Main St, Tishomingo, OK. Connect to internet to \nsee place info.",
+      htmlSnippet:
+        "Ruby Ranch Boutique, 316 W Main St, Tishomingo, <b>OK</b>. Connect to internet to <br>\nsee place info.",
+      cacheId: "0DJa_Tpo3SgJ",
+      formattedUrl:
+        "https://www.google.com/maps/search/?api=1&query...query...id...",
+      htmlFormattedUrl:
+        "https://www.<b>google</b>.com/maps/search/?api=1&amp;query...query...id...",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQUDTMfgWSotdHvbOKW4AmY1steqsilnDJS4q33zGx-d0AXvvxPiFY9Mvs",
+            width: "204",
+            height: "247",
+          },
+        ],
+        metatags: [
+          {
+            referrer: "origin",
+            "twitter:card": "summary",
+            "theme-color": "#3367d6",
+            viewport: "initial-scale=1.0, user-scalable=no",
+            google: "notranslate",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://maps.gstatic.com/tactile/basepage/pegman_sherlock.png",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Senor Lopez, 2130 S Division St, Guthrie, OK - Google Maps",
+      htmlTitle:
+        "Senor Lopez, 2130 S Division St, Guthrie, <b>OK</b> - <b>Google</b> Maps",
+      link: "https://www.google.com/maps/search/?api=1&query=Senor%20Lopez%2C%202130%20S%20Division%20St%2C%20Guthrie%2C%20OK&query_place_id=ChIJE6zlHp2NsYcRYCuWTbPVdB8",
+      displayLink: "www.google.com",
+      snippet:
+        "Senor Lopez, 2130 S Division St, Guthrie, OK. Your location. Trails. Dedicated \nlanes. Bicycle-friendly roads. Dirt/unpaved trails. Live traffic. Fast. Slow. 2000 km.",
+      htmlSnippet:
+        "Senor Lopez, 2130 S Division St, Guthrie, <b>OK</b>. Your location. Trails. Dedicated <br>\nlanes. Bicycle-friendly roads. Dirt/unpaved trails. Live traffic. Fast. Slow. 2000 km.",
+      cacheId: "P9hAzvdaX0cJ",
+      formattedUrl:
+        "https://www.google.com/maps/search/?api=1&query...query_place...",
+      htmlFormattedUrl:
+        "https://www.<b>google</b>.com/maps/search/?api=1&amp;query...query_place...",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQUDTMfgWSotdHvbOKW4AmY1steqsilnDJS4q33zGx-d0AXvvxPiFY9Mvs",
+            width: "204",
+            height: "247",
+          },
+        ],
+        metatags: [
+          {
+            referrer: "origin",
+            "twitter:card": "summary",
+            "theme-color": "#3367d6",
+            viewport: "initial-scale=1.0, user-scalable=no",
+            google: "notranslate",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://maps.gstatic.com/tactile/basepage/pegman_sherlock.png",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title:
+        "SMOKE. Woodfire Grill, 1542 East 15th Street, Tulsa, OK - Google ...",
+      htmlTitle:
+        "SMOKE. Woodfire Grill, 1542 East 15th Street, Tulsa, <b>OK</b> - <b>Google</b> ...",
+      link: "https://www.google.com/maps/search/?api=1&query=SMOKE.%20Woodfire%20Grill%2C%201542%20East%2015th%20Street%2C%20Tulsa%2C%20OK&query_place_id=ChIJwTGc35bstocRXr3EGXZ544w",
+      displayLink: "www.google.com",
+      snippet:
+        "SMOKE. Woodfire Grill, 1542 East 15th Street, Tulsa, OK. Connect to internet to \nsee place info.",
+      htmlSnippet:
+        "SMOKE. Woodfire Grill, 1542 East 15th Street, Tulsa, <b>OK</b>. Connect to internet to <br>\nsee place info.",
+      cacheId: "G4h_LUBoYKEJ",
+      formattedUrl:
+        "https://www.google.com/maps/search/?api=1&query=SMOKE...id...",
+      htmlFormattedUrl:
+        "https://www.<b>google</b>.com/maps/search/?api=1&amp;query=SMOKE...id...",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQUDTMfgWSotdHvbOKW4AmY1steqsilnDJS4q33zGx-d0AXvvxPiFY9Mvs",
+            width: "204",
+            height: "247",
+          },
+        ],
+        metatags: [
+          {
+            referrer: "origin",
+            "twitter:card": "summary",
+            "theme-color": "#3367d6",
+            viewport: "initial-scale=1.0, user-scalable=no",
+            google: "notranslate",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://maps.gstatic.com/tactile/basepage/pegman_sherlock.png",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "AMC CLASSIC Lawton 8, 7102 NW Cache Rd, Lawton, OK - Google ...",
+      htmlTitle:
+        "AMC CLASSIC Lawton 8, 7102 NW Cache Rd, Lawton, <b>OK</b> - <b>Google</b> ...",
+      link: "https://www.google.com/maps/search/?api=1&query=AMC%20CLASSIC%20Lawton%208%2C%207102%20NW%20Cache%20Rd%2C%20Lawton%2C%20OK&query_place_id=ChIJx9haqSgerYcRPE9DnnsN2-U",
+      displayLink: "www.google.com",
+      snippet:
+        "AMC CLASSIC Lawton 8, 7102 NW Cache Rd, Lawton, OK. Your location. Trails. \nDedicated lanes. Bicycle-friendly roads. Dirt/unpaved trails. Live traffic. Fast.",
+      htmlSnippet:
+        "AMC CLASSIC Lawton 8, 7102 NW Cache Rd, Lawton, <b>OK</b>. Your location. Trails. <br>\nDedicated lanes. Bicycle-friendly roads. Dirt/unpaved trails. Live traffic. Fast.",
+      cacheId: "doVktr47aTgJ",
+      formattedUrl:
+        "https://www.google.com/maps/search/?api=1&query...query...id...",
+      htmlFormattedUrl:
+        "https://www.<b>google</b>.com/maps/search/?api=1&amp;query...query...id...",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQUDTMfgWSotdHvbOKW4AmY1steqsilnDJS4q33zGx-d0AXvvxPiFY9Mvs",
+            width: "204",
+            height: "247",
+          },
+        ],
+        metatags: [
+          {
+            referrer: "origin",
+            "twitter:card": "summary",
+            "theme-color": "#3367d6",
+            viewport: "initial-scale=1.0, user-scalable=no",
+            google: "notranslate",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://maps.gstatic.com/tactile/basepage/pegman_sherlock.png",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Tulsa Transit, 510 S Rockford Ave, Tulsa, OK - Google Maps",
+      htmlTitle:
+        "Tulsa Transit, 510 S Rockford Ave, Tulsa, <b>OK</b> - <b>Google</b> Maps",
+      link: "https://www.google.com/maps/search/?api=1&query=Tulsa%20Transit%2C%20510%20S%20Rockford%20Ave%2C%20Tulsa%2C%20OK&query_place_id=ChIJh3Q4kojstocR_T_sIXGf0CM",
+      displayLink: "www.google.com",
+      snippet:
+        "Tulsa Transit, 510 S Rockford Ave, Tulsa, OK. Connect to internet to see place \ninfo.",
+      htmlSnippet:
+        "Tulsa Transit, 510 S Rockford Ave, Tulsa, <b>OK</b>. Connect to internet to see place <br>\ninfo.",
+      cacheId: "IT5yKsBLn0kJ",
+      formattedUrl:
+        "https://www.google.com/maps/search/?api=1&query...query...id...",
+      htmlFormattedUrl:
+        "https://www.<b>google</b>.com/maps/search/?api=1&amp;query...query...id...",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQUDTMfgWSotdHvbOKW4AmY1steqsilnDJS4q33zGx-d0AXvvxPiFY9Mvs",
+            width: "204",
+            height: "247",
+          },
+        ],
+        metatags: [
+          {
+            referrer: "origin",
+            "twitter:card": "summary",
+            "theme-color": "#3367d6",
+            viewport: "initial-scale=1.0, user-scalable=no",
+            google: "notranslate",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://maps.gstatic.com/tactile/basepage/pegman_sherlock.png",
+          },
+        ],
+      },
+    },
+  ],
+};
